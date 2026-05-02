9e4352b772244ff9b499806551ec7f6b1b6ee291
Edward Melendez <edward.melendez.mendigure@gmail.com>
2026-05-01 19:02:02 -0500

    upd: vars

--- config/var.ts
+++ config/var.ts
@@ -1,4 +1,9 @@
 export const variables = {
   BETTER_AUTH_URL: process.env.BETTER_AUTH_URL ?? "https://yalas-app.vercel.app",
   NEXT_PUBLIC_MOBILE_APP_URL: process.env.NEXT_PUBLIC_MOBILE_APP_URL ?? "",
+  GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID ?? "",
+  GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET ?? "",
+  APPLE_CLIENT_ID: process.env.APPLE_CLIENT_ID ?? "",
+  APPLE_CLIENT_SECRET: process.env.APPLE_CLIENT_SECRET ?? "",
+  APPLE_APP_BUNDLE_IDENTIFIER: process.env.APPLE_APP_BUNDLE_IDENTIFIER ?? "",
 }
